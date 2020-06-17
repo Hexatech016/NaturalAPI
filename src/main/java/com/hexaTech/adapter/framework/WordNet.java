b5e335b7a06c35c5e27a4ae1650f12c9720a5c55
aleBarbiero <barbiero.alessio.97@gmail.com>
2020-06-17 14:04:54 +0200

    Dynamic WordNet path.

--- src/main/java/com/hexaTech/adapter/framework/WordNet.java
+++ src/main/java/com/hexaTech/adapter/framework/WordNet.java
@@ -6,44 +6,87 @@ import net.didion.jwnl.JWNLException;
 import net.didion.jwnl.data.*;
 import net.didion.jwnl.dictionary.Dictionary;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 
 @Component
 public class WordNet implements WordParsingInterface {
 
-    public boolean thisNounIsASynonymOf(String word, String target) throws FileNotFoundException, JWNLException {
-        JWNL.initialize(new FileInputStream("."+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"properties.xml"));
-        final Dictionary dictionary = Dictionary.getInstance();
-        final IndexWord indexWord = dictionary.lookupIndexWord(POS.NOUN, target);
-        if(indexWord==null)
-            return false;
-        final Synset[] senses = indexWord.getSenses();
-        return check(senses,word);
+    static String jarName="/properties.xml";
 
-    }
+    public boolean thisNounIsASynonymOf(String word, String target) throws JWNLException {
+        try{
+            //replaceXML(jarName,"/dictio");
+            JWNL.initialize(new FileInputStream(jarName));
+        }catch(FileNotFoundException | NullPointerException e){
+            replaceXML("."+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"properties.xml",
+                    "./src/main/resources/dictio");
+            JWNL.initialize(new FileInputStream("."+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"properties.xml"));
+        }finally{
+            final Dictionary dictionary = Dictionary.getInstance();
+            final IndexWord indexWord = dictionary.lookupIndexWord(POS.NOUN, target);
+            if (indexWord == null)
+                return false;
+            final Synset[] senses = indexWord.getSenses();
+            return check(senses, word);
+        }
+    }//thisNounIsASynonymOf
 
-    public boolean thisVerbIsASynonymOf(String word, String target) throws FileNotFoundException, JWNLException {
-        JWNL.initialize(new FileInputStream("."+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"properties.xml"));
-        final Dictionary dictionary = Dictionary.getInstance();
-        final IndexWord indexWord = dictionary.lookupIndexWord(POS.VERB, target);
-        if(indexWord==null)
-            return false;
-        final Synset[] senses = indexWord.getSenses();
-        return check(senses,word);
-    }
+    public boolean thisVerbIsASynonymOf(String word, String target) throws JWNLException {
+        try{
+            //replaceXML(jarName,"/dictio");
+            JWNL.initialize(new FileInputStream(jarName));
+        }catch(FileNotFoundException | NullPointerException e){
+            replaceXML("."+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"properties.xml",
+                    "./src/main/resources/dictio");
+            JWNL.initialize(new FileInputStream("."+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"properties.xml"));
+        }finally{
+            final Dictionary dictionary = Dictionary.getInstance();
+            final IndexWord indexWord = dictionary.lookupIndexWord(POS.VERB, target);
+            if (indexWord == null)
+                return false;
+            final Synset[] senses = indexWord.getSenses();
+            return check(senses, word);
+        }
+    }//thisVerbIsASynonymOf
 
     private boolean check(Synset[] senses, String word) {
         for (Synset synset : senses) {
             Word[] words = synset.getWords();
             for (Word w : words) {
-                if (word.equalsIgnoreCase(w.getLemma())) {
+                if (word.equalsIgnoreCase(w.getLemma()))
                     return true;
-                }
-            }
-        }
+            }//for
+        }//for
         return false;
-    }
-}
+    }//check
+
+    private void replaceXML(String filepath,String toReplace) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(filepath);
+        NodeList staff = doc.getElementsByTagName("param");
+        for(int i=0;i<staff.getLength();i++){
+            NamedNodeMap attr =staff.item(i).getAttributes();
+            Node nodeAttr=attr.getNamedItem("name");
+            if(nodeAttr.getTextContent().equalsIgnoreCase("dictionary_path")) {
+                Node nodeVal=attr.getNamedItem("value");
+                nodeVal.setTextContent(toReplace);
+            }//if
+        }//for
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(filepath));
+        transformer.transform(source, result);
+    }//replaceXML
+
+}//WordNet
